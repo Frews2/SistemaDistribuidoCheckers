982c230e6aa4edb33afabc4502973c0c8c1be6b8
Rick <51493230+Frews2@users.noreply.github.com>
2020-11-19 15:56:08 -0600

    Create INSERTINTOJugador.sql

---
+++ ServidorCheckers/INSERTINTOJugador.sql
@@ -0,0 +1,5 @@
+﻿INSERT INTO Jugador(apodo,contrasenia,correoElectronico, status, respuestaConfirmacion,preguntaRecuperacion, pinConfirmacion, idCreador, idioma) 
+VALUES ('frews2','frews1','zS18012183@estudiantes.uv.mx','Activo','negro','Como te gusta tu cafe?','4141',1,1),
+('pklove3456','bobcholo','zS18012143@estudiantes.uv.mx','Activo','yo','Quien es el patron?','1234',1,1),
+('miracleJack','whatsgood','JJmiraclex@gmail.com','Activo','everything','Whats good in the hood?','5342',1,2),
+('alfredo fredo','ilventodoro','fredo777@outlook.com','Activo','firenze','citta preferita?','7771',1,3)
